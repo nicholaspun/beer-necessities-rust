3f622f0bbf8c9c242c76365698d06582b0e6734b
Nicholas Pun <nicholaspun99@gmail.com>
2018-11-01 19:49:27 -0400

    add beer routes and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+#[macro_use] extern crate serde_derive;
 
+extern crate serde;
+extern crate serde_json;
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::response::status;
+use rocket::http::Status;
+
+struct BeerEntry {
+    id: u32,
+    brewery: String,
+    name: String,
+    description: String,
+    aroma: String,
+    taste: String,
+    finish: String,
+    rating: u8
 }
 
+#[get("/")]
+fn index() -> status::Accepted<String> {
+    status::Accepted(Some(format!("Ok")))
+}
+
+#[get("/beers")]
+fn get_beers() -> status::Accepted<String> {
+    status::Accepted(Some(format!("Ok")))
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, get_beers]).launch();
 }
